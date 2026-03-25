7b9b437c5120cf32e8226628a05fbc6cd7f2763d
Hamzakhan001 <spaceus9@gmail.com>
2026-03-25 20:56:55 +0000

    themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter, Geist } from "next/font/google";
 import "@/assets/styles/globals.css";
 import { cn } from "@/lib/utils";
+import { ThemeProvider } from "next-themes";
 
 const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
@@ -19,10 +20,20 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
+      lang="en" suppressHydrationWarning
       className={cn("h-full", "antialiased", inter.className, "font-sans", geist.variable)}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <ThemeProvider
+
+        attribute="class"
+        defaultTheme="light"
+        enableSystem
+        disableTransitionOnChange
+        >
+        {children}
+        </ThemeProvider>
+        </body>
     </html>
   );
 }
--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { APP_NAME } from "@/lib/constants";
+import ModeToggle from "./mode-toggle";
 
 const Header = () => {
   return (
@@ -18,12 +19,13 @@ const Header = () => {
         </div>
 
         <div className="space-x-2">
+            <ModeToggle />
             <Button asChild variant='ghost'>
                 <Link href="/cart" className="flex items-center gap-2">
                     <ShoppingCart /> Cart
                 </Link>
             </Button>
-            <Button asChild variant='ghost'>
+            <Button asChild >
                 <Link href="/signin" className="flex items-center gap-2">
                     <UserIcon /> Sign In
                 </Link>
